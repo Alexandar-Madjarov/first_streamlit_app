331b9334aa1cb03c95a10c03f8ed34fcaca1b0a6
Alexandar-Madjarov <41257948+Alexandar-Madjarov@users.noreply.github.com>
2022-06-15 13:13:45 +0200

    Update streamlit_app.py
    add more recipes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents'' New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and bluberry oatmeal')
+streamlit.text('Kale bla bla')
+streamlit.text('Third recipe')
